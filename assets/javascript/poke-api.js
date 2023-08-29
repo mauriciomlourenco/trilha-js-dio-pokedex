6a916a656fc8eccdddc551ea8f2b99594c6001bf
Mauricio Machado Lourenço <mauricio.mchd@gmail.com>
2023-08-28 23:43:44 -0300

    List of Pokémon

---
+++ assets/javascript/poke-api.js
@@ -0,0 +1,37 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types
+    
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default;
+
+    return pokemon;
+}
+
+pokeApi.getPokeonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon);
+
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((response) => response.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokeonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
+        .catch((error) => console.error(error))
+        .finally(() => console.log('Requisição concluída'));
+}
